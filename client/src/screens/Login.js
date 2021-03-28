c7fb9d4ac222cace827bb42650967cec553af862
Caint-dev <69853511+Caint-dev@users.noreply.github.com>
2021-03-28 16:57:38 +0200

    update Login.js and Register.js
    Making of the connection part in the client side

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React,{useState}  from 'react';
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import loginStyle from '../styles/authentication/login'
 
 const path = "../assets/images/authentication/login/"
-const Login = () => {
+const Login = (props) => {
+
+    const [email,setEmail] =  useState('');
+  const [password,setPassword]= useState('')
+    
+  
+  const sendCred = async (props)=>{
+    fetch("http://192.168.1.25:4547/NearToMe/signin",{
+      method:"POST",
+      headers: {
+       'Content-Type': 'application/json'
+     },
+     body:JSON.stringify({
+       "email":email,
+       "password":password
+     })
+    })
+    .then(res=>res.response())
+    .then(async (data)=>{
+           try {
+           } catch (e) {
+             console.log("error ",e)
+              Alert(e)
+           }
+    })}
     return (
         <View style={loginStyle.form}>
             <View style={loginStyle.content}>
                 <Image source={require(path + "loginIcon.png")} style={loginStyle.icon}></Image>
                 <Text style={loginStyle.txt}>Adresse mail</Text>
-                <TextInput style={loginStyle.mailInput}></TextInput>
+                <TextInput value={email} 
+                    style={loginStyle.mailInput} 
+                    onChangeText={(text)=>setEmail(text.trim())}>
+                </TextInput>
                 <Text style={loginStyle.txt}>Mot de passe</Text>
                 <View style={loginStyle.pwdSection}>
-                    <TextInput style={loginStyle.pwdInput}></TextInput>
+                    <TextInput secureTextEntry={true} 
+                        value={password}
+                        onChangeText={(text)=>{setPassword(text.trim())}} 
+                        style={loginStyle.pwdInput} >
+                    </TextInput>
                     <TouchableOpacity style={loginStyle.hidePwdButton}>
                         <Image source={require(path + "hideLight.png")} style={loginStyle.hidePwdIcon}></Image>
                     </TouchableOpacity>
                 </View>
-                <View style={loginStyle.loginButtonSection}>
-                    <TouchableOpacity style={loginStyle.loginButton}>
+                <View style={loginStyle.loginButtonSection} >
+                    <TouchableOpacity style={loginStyle.loginButton}   onPress={() => sendCred(props)}>
                         <Text style={loginStyle.loginButtonText}>Se connecter</Text>
-                        <Image source={require(path + "proceedDark.png")} style={loginStyle.loginButtonIcon}></Image>
+                        <Image source={require(path + "proceedDark.png")} style={loginStyle.loginButtonIcon} ></Image>
                     </TouchableOpacity>
                     <Text style={loginStyle.forgetPwdText}>Mot de passe oublié ?</Text>
                 </View>
--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React,{useState} from "react";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native"
 
 import registerStyle from '../styles/authentication/register'
 
-const path = "../assets/images/authentication/register/"
-export default Register = () => {
+
+export default Register = (props) => {
+    const path = "../assets/images/authentication/register/"
+    const [email,setEmail] = useState('');
+    const [password,setPassword]=useState('')
+    const [name,setName] = useState('');
+    const [firstName,setFirstName] = useState('');
+    const [phone,setPhone]=useState('')
+
+    const sendCred = async (props)=>{
+        fetch("http://192.168.1.25:4547/NearToMe/signup",{
+          method:"POST",
+          headers: {
+           'Content-Type': 'application/json'
+         },
+         body:JSON.stringify({
+           "email":email,
+           "password":password,
+           "name":name,
+           "firstName":firstName,
+           "phone":phone
+         })
+        })
+        .then(res=>res.json())
+        .then(async (data)=>{
+               try {
+                 props.navigation.replace("home")
+               } catch (e) {
+                 console.log("error",e)
+               }
+        })
+     }
     return (
         <View style={registerStyle.form}>
             <View style={registerStyle.content}>
@@ -14,22 +44,42 @@ export default Register = () => {
                 </TouchableOpacity>
                 <Image source={require(path + "registerIcon.png")} style={registerStyle.icon}></Image>
                 <Text style={registerStyle.text}>Prénom</Text>
-                <TextInput style={registerStyle.input}></TextInput>
+                <TextInput  
+                    value={firstName}
+                    onChangeText={(text)=>setFirstName(text.trim())}
+                    style={registerStyle.input}>
+                </TextInput>
                 <Text style={registerStyle.text}>Nom</Text>
-                <TextInput style={registerStyle.input}></TextInput>
+                <TextInput  
+                    value={name}
+                    onChangeText={(text)=>setName(text.trim())}
+                    style={registerStyle.input}>
+                </TextInput>                
                 <Text style={registerStyle.text}>Téléphone</Text>
-                <TextInput style={registerStyle.input}></TextInput>
+                <TextInput  
+                    value={phone}
+                    onChangeText={(text)=>setPhone(text.trim())}
+                    style={registerStyle.input}>
+                </TextInput>                  
                 <Text style={registerStyle.text}>Adresse mail</Text>
-                <TextInput style={registerStyle.input}></TextInput>
+                <TextInput  
+                    value={email}
+                    onChangeText={(text)=>setEmail(text.trim())}
+                    style={registerStyle.input}>
+                </TextInput>                
                 <Text style={registerStyle.text}>Mot de passe</Text>
                 <View style={registerStyle.passwordSection}>
-                    <TextInput secureTextEntry={true} style={registerStyle.passwordInput}></TextInput>
-                    <TouchableOpacity style={registerStyle.passwordHideButton}>
+                <TextInput  
+                    value={password}
+                    onChangeText={(text)=>setPassword(text.trim())}
+                    style={registerStyle.input}>
+                </TextInput>                   
+                <TouchableOpacity style={registerStyle.passwordHideButton}>
                         <Image source={require(path + "hideDark.png")} style={registerStyle.passwordHideButtonIcon}></Image>
-                    </TouchableOpacity>
+                </TouchableOpacity>
                 </View>
                 <View style={registerStyle.registerButtonArea}>
-                    <TouchableOpacity style={registerStyle.registerButton}>
+                    <TouchableOpacity style={registerStyle.registerButton}  onPress={() => sendCred(props)}>
                         <Text style={registerStyle.registerButtonTxt}>S'inscrire</Text>
                         <Image source={require(path + "proceedLight.png")} style={registerStyle.registerButtonIcon}></Image>
                     </TouchableOpacity>
