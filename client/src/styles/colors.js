f2768e968542c582d9aaf36db549eb6180cdb25c
Mathieu RODRIGUES <mathieudomingues925@gmail.com>
2021-03-29 14:12:46 +0200

    Update colors.js
    Ajout de la couleur "light_gray"

--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -2,6 +2,8 @@ export const primaryColor = "#FF4A4A"
 
 export const white = "#FFFFFF";
 
+export const light_gray = "#f8f8f8";
+
 export const black = "#000000";
 
 export const light_red = "#FF7878";
